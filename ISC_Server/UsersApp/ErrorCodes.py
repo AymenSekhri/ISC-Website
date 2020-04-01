2869213c12400b840dacefced94930c00eaa5a46
AymenSekhri <aymen.sekhri@live.fr>
2020-04-01 11:30:00 +0100

    Security : fixning the returning status error to not reveal too much
    In register, if user exists or email exists return same error.
    In Login, if wrong pass or email not found return same error.

--- ISC_Server/UsersApp/ErrorCodes.py
+++ ISC_Server/UsersApp/ErrorCodes.py
@@ -4,13 +4,13 @@ class ErrorCodes(object):
     class REGISTER_INPUTS(object):
         NONE = 0
         USEREXISTS = 1
-        EMAILEXISTS = 2
+        EMAILEXISTS = 1
         PASSMISSMATCH = 3
 
     class LOGIN_INPUTS(object):
         NONE = 0
         EMAIL_NOT_FOUND = 1
-        PASS_MISMATCH = 2
+        PASS_MISMATCH = 1
 
     class FORGOT_INPUTS(object):
         NONE = 0
